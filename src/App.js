b919647b70499efe5fececec185facd8f990e5e4
Muzealth <johnmuzanyi@gmail.com>
2024-11-11 21:23:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to [Your Business Name]</h1>
+        <p>Converting customers from social media platforms</p>
+        <a
+          href="https://facebook.com/yourbusiness"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Facebook
+        </a>
+        <a
+          href="https://twitter.com/yourbusiness"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://instagram.com/yourbusiness"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
