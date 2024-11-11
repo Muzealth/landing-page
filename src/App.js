2a7b9cfea093b56b079164ff0735b263c427bef2
Muzealth <johnmuzanyi@gmail.com>
2024-11-11 21:57:27 +0300

    Added new changes

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to [Your Business Name]</h1>
+        <h1>Welcome to [Muzealth Supplements]</h1>
         <p>Converting customers from social media platforms</p>
         <a
-          href="https://facebook.com/yourbusiness"
+          href="https://facebook.com/muzealth-supplements"
           target="_blank"
           rel="noopener noreferrer"
         >
           Facebook
         </a>
         <a
-          href="https://twitter.com/yourbusiness"
+          href="https://twitter.com/muzealth-supplements"
           target="_blank"
           rel="noopener noreferrer"
         >
           Twitter
         </a>
         <a
-          href="https://instagram.com/yourbusiness"
+          href="https://instagram.com/muzealth-supplements"
           target="_blank"
           rel="noopener noreferrer"
         >
